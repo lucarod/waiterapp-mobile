50f0accb0f72e7e7c19e4448f5d335c401cec73f
Luca Rodrigues <luca.rodrigues@wundermanthompson.com>
2023-11-28 23:03:46 -0300

    feat: finish header and add categories

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,10 +1,32 @@
-import { Container } from './styles';
+import { Categories } from '../components/Categories';
 import { Header } from '../components/Header';
+import { Menu } from '../components/Menu';
+
+import {
+  Container,
+  CategoriesContainer,
+  MenuContainer,
+  Footer,
+  FooterContainer,
+} from './styles';
 
 export function Main() {
   return (
-    <Container>
-      <Header />
-    </Container>
+    <>
+      <Container>
+        <Header />
+        <CategoriesContainer>
+          <Categories />
+        </CategoriesContainer>
+        <MenuContainer>
+          <Menu />
+        </MenuContainer>
+      </Container>
+      <Footer>
+        <FooterContainer>
+
+        </FooterContainer>
+      </Footer>
+    </>
   );
 }
--- src/Main/styles.ts
+++ src/Main/styles.ts
@@ -4,5 +4,22 @@ import { Platform, StatusBar } from 'react-native';
 const isAndroid = Platform.OS === 'android';
 
 export const Container = styled.SafeAreaView`
+  flex: 1;
   margin-top: ${isAndroid ? `${StatusBar.currentHeight}px` : '0' };
+  background: #FAFAFA;
 `;
+
+export const CategoriesContainer = styled.View`
+  height: 73px;
+  margin-top: 34px;
+`;
+
+export const MenuContainer = styled.View`
+  flex: 1;
+`;
+
+export const Footer = styled.View`
+  min-height: 110px;
+`;
+
+export const FooterContainer = styled.SafeAreaView``;
